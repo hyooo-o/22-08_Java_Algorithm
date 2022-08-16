9303bbecb79312878ecffb0c8263adf1964b20e7
hyooo-o <xozo5939@gmail.com>
2022-08-17 00:52:36 +0900

    0816_edit

--- 0816/src/Main_1074_김정효.java
+++ 0816/src/Main_1074_김정효.java
@@ -2,30 +2,40 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
+/**
+ * BOJ 1074 Z
+ * 이분 탐색 => 사분면 나눠서 생각하기
+ * @author kjh
+ *
+ */
 public class Main_1074_김정효 {
-
+	static int cnt;
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
 		int r = Integer.parseInt(st.nextToken());
 		int c = Integer.parseInt(st.nextToken());
-		
-		
-		
+		binarySearch((int) Math.pow(2, n), r, c);
+		System.out.println(cnt);
 	}
 	
-	private static int binarySearch(int[][] map, int[][] key) {
-		int start=0;
-		int end = map.length;
-		
-		while(start <= end) {
-			int mid = (start+end) / 2;
-			int x_mid = mid;
+	private static void binarySearch(int size, int r, int c) {
+		int mid = size / 2;
+		if (size == 1) return;
+		else if (r < mid && c < mid) {	// 1사분면
+			binarySearch(mid, r, c);
+		}else if (r < mid && c >= mid) {	// 2사분면
+			cnt += (size*size) / 4;
+			binarySearch(mid, r, c-mid);
+		}else if (r >= mid && c < mid) {	// 3사분면
+			cnt += (size*size/4) * 2;
+			binarySearch(mid, r-mid, c);
+		}else if (r >= mid && c >= mid) {	// 4사분면
+			cnt += (size*size/4) * 3;
+			binarySearch(mid, r-mid, c-mid);
 		}
-		
-		return 0;
 	}
 
 }
--- 0816/src/Main_1934_김정효.java
+++ 0816/src/Main_1934_김정효.java
@@ -1,14 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 /**
  * BOJ 1934 최소공배수
- * 
+ * 1. gcd로 최대공약수 구하기
+ * 2. 두 수의 곱 / 최대공약수 => 최소공배수 
  * @author kjh
  *
  */
 public class Main_1934_김정효 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	static int result;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		
+		for(int i=0; i<t; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			gcd(a, b);
+			System.out.println(a*b/result);
+		}
 	}
+
+	private static void gcd(int a, int b) {
+		if (a % b == 0) {
+			result = b;
+		}else {
+			gcd(b, a%b);
+		}
+		
+	}
+	
+	
 
 }
