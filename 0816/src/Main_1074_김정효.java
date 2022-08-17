9fa8f5953bc25207200df5191a327dd14f4b9c7b
hyooo-o <72725382+hyooo-o@users.noreply.github.com>
2022-08-17 09:14:00 +0900

    Update Main_1074_김정효.java

--- 0816/src/Main_1074_김정효.java
+++ 0816/src/Main_1074_김정효.java
@@ -24,9 +24,9 @@ public class Main_1074_김정효 {
 	private static void binarySearch(int size, int r, int c) {
 		int mid = size / 2;
 		if (size == 1) return;
-		else if (r < mid && c < mid) {	// 1사분면
+		else if (r < mid && c < mid) {	// 2사분면
 			binarySearch(mid, r, c);
-		}else if (r < mid && c >= mid) {	// 2사분면
+		}else if (r < mid && c >= mid) {	// 1사분면
 			cnt += (size*size) / 4;
 			binarySearch(mid, r, c-mid);
 		}else if (r >= mid && c < mid) {	// 3사분면
